a0581e2d36f34443041ce49cc840ec3871db98df
Khowshikksharma <2200031954@kluniversity.in>
2025-04-28 09:21:22 +0530

    updated Jenkinsfile, dockerfile. added .dockerignore file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,28 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 3.0.2"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
 }
 
-resource "docker_container" "nginx_server" {
-  image = docker_image.nginx.latest
-  name  = "nginx_server"
+resource "docker_image" "healthcaremrs" {
+  name = "khowshikksharma/healthcaremrs:latest"
+}
+
+resource "docker_container" "healthcaremrs_container" {
+  name  = "healthcaremrs-app"
+  image = docker_image.healthcaremrs.name
+
   ports {
     internal = 80
-    external = 8080
+    external = 8090
+    ip       = "0.0.0.0"
+    protocol = "tcp"
   }
 }
